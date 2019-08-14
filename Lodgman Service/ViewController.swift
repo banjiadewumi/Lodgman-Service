6a9e23eb7da14c9d85d5b522b716e5c962694814
Banji Adewumi <banji@Banjis-MacBook-Air.local>
2019-08-14 11:18:10 +0300

    add global clock

--- Lodgman Service/ViewController.swift
+++ Lodgman Service/ViewController.swift
@@ -14,15 +14,17 @@ class ViewController: UIViewController {
     let watch = StopWatch()
     
     @IBOutlet weak var timerLbl: UILabel!
-    
+    @IBOutlet weak var dayTimeLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateElapsedTimeLabel(timer:)), userInfo: nil, repeats: true)
+        
         
         watch.start()
+        timeOfDay()
+        updateAllTime()
     }
     
     
@@ -48,8 +50,18 @@ class ViewController: UIViewController {
     
     }
     
-    func addFunction(){
+    func updateAllTime() {
         
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateElapsedTimeLabel(timer:)), userInfo: nil, repeats: true)
+        
+        Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(timeOfDay), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timeOfDay(){
+        
+        let dayTime = DateFormatter.localizedString(from: Date(), dateStyle: DateFormatter.Style.none, timeStyle: DateFormatter.Style.short)
+        
+        dayTimeLbl.text = dayTime
     }
     
     
