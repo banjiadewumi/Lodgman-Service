caf4f54eda7ff2ffce0d38adfeb14e01dec0c605
Banji Adewumi <banji@Banjis-MacBook-Air.local>
2019-08-14 17:48:21 +0300

    added timeSinceLastBusLeft

--- Lodgman Service/ViewController.swift
+++ Lodgman Service/ViewController.swift
@@ -12,15 +12,16 @@ class ViewController: UIViewController {
     
     
     let watch = StopWatch()
+    var dayTime: String = ""
     
     @IBOutlet weak var timerLbl: UILabel!
     @IBOutlet weak var dayTimeLbl: UILabel!
+    @IBOutlet weak var timeLastBusLeft: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
         
         watch.start()
         timeOfDay()
@@ -50,20 +51,39 @@ class ViewController: UIViewController {
     
     }
     
+    
+    // MARK: update all the times stamps
     func updateAllTime() {
         
         Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateElapsedTimeLabel(timer:)), userInfo: nil, repeats: true)
         
         Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(timeOfDay), userInfo: nil, repeats: true)
+        
+       
     }
     
+    
+    // The accurate time of the day!
     @objc func timeOfDay(){
         
-        let dayTime = DateFormatter.localizedString(from: Date(), dateStyle: DateFormatter.Style.none, timeStyle: DateFormatter.Style.short)
+        dayTime = DateFormatter.localizedString(from: Date(), dateStyle: DateFormatter.Style.none, timeStyle: DateFormatter.Style.short)
         
         dayTimeLbl.text = dayTime
+         timeSinceLastBusLeft()
     }
     
+    func timeSinceLastBusLeft() {
+     
+        switch dayTime {
+        case "8:00 AM", "8:30 AM", "9:00 AM","9:30 AM","10:00 AM","10:30 AM","11:00 AM","11:30 AM",
+             "12:00 PM","1:00 PM","2:00 PM","3:00 PM","3:30 PM","4:00 PM","4:30 PM",
+             "5:00 PM","5:30 PM","6:00 PM","7:00 PM","8:00 PM":
+            timeLastBusLeft.text = dayTime
+        default:
+            return
+        
+        }
+    }
     
 
 }
