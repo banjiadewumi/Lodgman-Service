28d8ea363c842e010a83158c846bdfa9d8ce6572
Banji Adewumi <banji@Banjis-MacBook-Air.local>
2019-08-18 22:41:47 +0300

    timer refactored

---
+++ Lodgman Service/ScheduleViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ScheduleViewController.swift
+//  Lodgman Service
+//
+//  Created by Banji Adewumi on 8/18/19.
+//  Copyright © 2019 Banji Adewumi. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func backButtonPressed() {
+    
+        dismiss(animated: true, completion: nil)
+    }
+
+}
--- Lodgman Service/ViewController.swift
+++ Lodgman Service/ViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    let watch = StopWatch()
     var dayTime: String = ""
     var nextBus: String = ""
+    var timer = Timer()
     
     @IBOutlet weak var timerLbl: UILabel!
     @IBOutlet weak var dayTimeLbl: UILabel!
@@ -25,9 +25,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        watch.start()
+
         timeOfDay()
-        updateAllTime()
+        updateGlobaltime()
+        
     }
     
     
@@ -39,30 +40,34 @@ class ViewController: UIViewController {
         present(controller, animated: true, completion: nil)
     }
     
-    // MARK: - Update Timer Label
-    @objc func updateElapsedTimeLabel (timer: Timer){
-        
-        if watch.isRunning {
-            let minutes = Int(watch.elapsedTime / 60)
-            let seconds = Int(watch.elapsedTime) % 60
-            timerLbl.text = String(format: "%02d:%02d", minutes, seconds)
-        }
-        else {
-            timer.invalidate()
-        }
     
+    
+    
+    // MARK: - Create a timner
+    func recurringTimer(){
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timeResults(timer:)), userInfo: Date(), repeats: true)
+        
+    }
+    
+    @objc func timeResults(timer: Timer){
+        let timerStart = timer.userInfo as! Date
+        let seconds = Int(Date().timeIntervalSince(timerStart)) % 60
+        let minutes = Int(Date().timeIntervalSince(timerStart)) / 60
+        timerLbl.text = String(format: "%02d:%02d", minutes, seconds)
+        
     }
     
     
     // MARK: update all the times stamps
-    func updateAllTime() {
+    func updateGlobaltime() {
         
-        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateElapsedTimeLabel(timer:)), userInfo: nil, repeats: true)
         
-        Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(timeOfDay), userInfo: nil, repeats: true)
+       Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(timeOfDay), userInfo: nil, repeats: true)
         
-       
     }
+    
+    
     
     
     // The accurate time of the day!
@@ -77,24 +82,35 @@ class ViewController: UIViewController {
     func timeSinceLastBusLeft() {
      
         switch dayTime {
-        case "8:00 AM", "8:30 AM", "9:00 AM","9:30 AM","10:00 AM","10:30 AM","11:00 AM","11:30 AM",
+        case "6:57 AM", "8:30 AM", "9:00 AM","9:53 AM","10:54 AM","10:30 AM","11:00 AM","11:30 AM",
              "12:00 PM","1:00 PM","2:00 PM","3:00 PM","3:30 PM","4:00 PM","4:30 PM",
-             "5:00 PM","5:30 PM","6:00 PM","7:00 PM","11:50 PM":
-            timeLastBusLeft.text = dayTime
+             "5:00 PM","5:30 PM","6:00 PM","7:00 PM","2:30 PM":
             
-            print(dayTime)
+            let frozenTime = dayTime
+            timeLastBusLeft.text = frozenTime
+            recurringTimer()
             
             let minutesToAdd: TimeInterval = 30 * 60
             let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "HH:mm a"
-            let date = dateFormatter.date(from: dayTime)
-            let datina = dateFormatter.string(from: date!)
-            print(datina)
-        
+            dateFormatter.dateFormat = "h:mm a"
+            let date = dateFormatter.date(from: frozenTime)
+            let nextBusTime = date?.addingTimeInterval(minutesToAdd)
+            nextBus = dateFormatter.string(from: nextBusTime ?? Date())
+            nextBusLbl.text = nextBus
         default:
             return
         
         }
+    }
+    
+    
+    // MARK: bus Schedule transition
+    @IBAction func busSchedulebutton() {
+        
+        let busSchedule: ScheduleViewController
+        busSchedule = storyboard?.instantiateViewController(withIdentifier: "ScheduleViewController") as! ScheduleViewController
+        
+        present(busSchedule, animated: true, completion: nil)
     }
     
 
